fce48c7cb418a9986fea6a1d2d66d6fa80dd04df
Sebastian Gibb <mail@sebastiangibb.de>
2016-06-17 09:46:54 +0200

    add first methods for FlexmapSet

---
+++ R/AllGenerics.R
@@ -0,0 +1,4 @@
+setGeneric("dilutionData", function(object) standardGeneric("dilutionData"))
+setGeneric("dilutionNames", function(object) standardGeneric("dilutionNames"))
+setGeneric("replicateData", function(object) standardGeneric("replicateData"))
+setGeneric("replicateNames", function(object) standardGeneric("replicateNames"))
---
+++ R/methods-FlexmapSet.R
@@ -0,0 +1,138 @@
+setMethod("initialize", "FlexmapSet",
+          function(.Object, assayData, phenoData, featureData, dilutionData,
+                   replicateData, exprs=new("array"), ... ) {
+  if (missing(assayData) && missing(exprs)) {
+    stop("provide at most one of 'assayData' or 'exprs' to initialize FlexmapSet",
+         call.=FALSE)
+  }
+  if (!missing(assayData)) {
+    if (is.null(assayData[["exprs"]])) {
+      stop("if 'assayData' is given it has to contain an 'exprs' array")
+    }
+    exprs <- assayData[["exprs"]]
+  }
+
+  if (missing(phenoData)) {
+    phenoData <- .annotatedDataFrameFromArray(exprs, dimname="sample")
+  }
+  if (missing(featureData)) {
+    featureData <- .annotatedDataFrameFromArray(exprs, dimname="feature")
+  }
+  if (missing(dilutionData)) {
+    dilutionData <- .annotatedDataFrameFromArray(exprs, dimname="dilution")
+  }
+  if (missing(replicateData)) {
+    replicateData <- .annotatedDataFrameFromArray(exprs, dimname="replicate")
+  }
+
+  if (!missing(assayData)) {
+    callNextMethod(.Object, assayData=assayData, phenoData=phenoData,
+                   featureData=featureData, dilutionData=dilutionData,
+                   replicateData=replicateData, ...)
+  } else {
+    callNextMethod(.Object, phenoData=phenoData, featureData=featureData,
+                   dilutionData=dilutionData, replicateData = replicateData,
+                   exprs=exprs, ...)
+  }
+})
+
+setValidity("FlexmapSet", function(object) {
+  msg <- NULL
+
+  if (!isCurrent(object)[["FlexmapSet"]]) {
+    msg <- validMsg(msg, "out-of-date class version 'FlexmapSet'")
+  }
+  dims <- dims(object)
+
+  if (ncol(dims) > 0L) {
+    ## dilutionData
+    if (dims[.dim["dilution"], 1L] != dim(dilutionData(object))[[1L]]) {
+      msg <- validMsg(msg,
+        "dilution numbers differ between assayData and dilutionData")
+    }
+    if (!identical(dilutionNames(object), rownames(dilutionData(object)))) {
+      msg <- validMsg(msg,
+        "dilutionNames differ between assayData and dilutionData")
+    }
+    ## replicateData
+    if (dims[.dim["replicate"], 1L] != dim(replicateData(object))[[1L]]) {
+      msg <- validMsg(msg,
+        "replicate numbers differ between assayData and replicateData")
+    }
+    if (!identical(replicateNames(object), rownames(replicateData(object)))) {
+      msg <- validMsg(msg,
+        "replicateNames differ between assayData and replicateData")
+    }
+  }
+  if (is.null(msg)) TRUE else msg
+})
+
+setMethod("dilutionData", "FlexmapSet", function(object) object@dilutionData)
+
+setMethod("dilutionNames", "FlexmapSet", function(object) {
+  dimnames(object)[[.dim["dilution"]]]
+})
+
+setMethod("dim", signature(x="FlexmapSet"),
+          function(x) {
+  d <- callNextMethod(x)
+  names(d) <- .Dimnames()
+  d
+})
+
+setMethod("dims", signature(object="FlexmapSet"),
+          function(object) {
+  d <- callNextMethod(object)
+  rownames(d) <- .Dimnames()
+  d
+})
+
+setMethod("dims", signature(object="FlexmapSet"),
+          function(object) {
+  d <- callNextMethod(object)
+  rownames(d) <- .Dimnames()
+  d
+})
+
+setMethod("dimnames", signature(x="FlexmapSet"),
+          function(x) {
+  dimnames(assayDataElement(x, "exprs"))
+})
+
+setMethod("exprs", signature(object="FlexmapSet"),
+          function(object) assayDataElement(object, "exprs"))
+
+setReplaceMethod("exprs", signature(object="FlexmapSet", value="array"),
+                 function(object, value) {
+  assayDataElementReplace(object, "exprs", value)
+})
+
+setMethod("replicateData", "FlexmapSet", function(object) object@replicateData)
+
+setMethod("replicateNames", "FlexmapSet", function(object) {
+  dimnames(object)[[.dim["replicate"]]]
+})
+
+setMethod("show", "FlexmapSet", function(object) {
+  cat(class(object), " (storageMode: ", storageMode(object), ")\n", sep="")
+
+  adim <- dim(object)
+
+  if (length(adim) > 1L) {
+    cat("assayData:", paste(adim, tolower(names(adim)), collapse=", "), "\n")
+    cat("  element names:", paste(assayDataElementNames(object),
+                                  collapse=", "), "\n")
+    cat(.showAnnotatedDataFrame(protocolData(object), "protocolData"), "\n")
+    cat(.showAnnotatedDataFrame(phenoData(object), "phenoData"), "\n")
+    cat(.showAnnotatedDataFrame(featureData(object), "featureData"), "\n")
+    cat(.showAnnotatedDataFrame(dilutionData(object), "dilutionData"), "\n")
+    cat(.showAnnotatedDataFrame(replicateData(object), "replicateData"), "\n")
+
+    cat("experimentData: use 'experimentData(object)'\n")
+
+    pmids <- pubMedIds(object)
+    if (length(pmids) && all(nzchar(pmids))) {
+      cat("  pubMedIds:", paste(pmids, sep=", "), "\n")
+    }
+  }
+})
